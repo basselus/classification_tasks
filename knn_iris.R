34f3f808adfd3c5baba4afcc0b03d7caa3d70a71
basselus <Mozes-live@hotmail.fr>
2017-10-24 14:35:30 +0200

    some adds to the EDA step

--- knn_iris.R
+++ knn_iris.R
@@ -55,6 +55,30 @@ xyplot(Petal.Length ~ Petal.Width, data=iris, groups=Species,
        auto.key=list(x=0.15, y=0.85, bty="l")
 )
 
+#Print the correlation matrixes for each of the classes of the target variable:
+#(Setosa, Versicolor,Virginica)
+
+#overall correlation between numeric variables
+cor(iris[,1:4]) 
+
+# Return the values of iris levels
+x=levels(iris$Species) 
 
 
+#Print all the correlation matrixes by classes
+# Setosa correlation matrix
+print(x[1]) 
+cor(iris[iris$Species==x[1],1:4])
+
+# Versicolor correlation matrix
+print(x[2]) 
+cor(iris[iris$Species==x[2],1:4])
+
+# Virginica correlation matrix
+print(x[3]) 
+cor(iris[iris$Species==x[3],1:4])
+
+#look how data is shaped and distributed around species
+round(prop.table(table(iris$Species)*100), digits=1)
+summary(iris)
 
